e2fc5c141929e70914e1e2c1c7f6de15812c7e7a
RogerMi12 <82840097+RogerMi12@users.noreply.github.com>
2025-04-28 19:53:54 -0500

    Create Estadio.java

---
+++ project/src/main/java/com/spring/project/entity/Estadio.java
@@ -0,0 +1,29 @@
+package com.spring.project.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "estadio")
+public class Estadio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String nombre;
+    private int capacidad;
+
+    // Getters y Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public int getCapacidad() { return capacidad; }
+    public void setCapacidad(int capacidad) { this.capacidad = capacidad; }
+}
