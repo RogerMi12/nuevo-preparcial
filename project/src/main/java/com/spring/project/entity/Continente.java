b20e0a7e3851ee23c06b7fe35d8293c978076ed9
RogerMi12 <82840097+RogerMi12@users.noreply.github.com>
2025-04-28 19:53:37 -0500

    Create Continente.java

---
+++ project/src/main/java/com/spring/project/entity/Continente.java
@@ -0,0 +1,25 @@
+package com.spring.project.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "continente")
+public class Continente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String nombre;
+
+    // Getters y Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+}
