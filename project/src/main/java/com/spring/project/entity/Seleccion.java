d7a67c6b0eed9a9bf284e7e60fd935972bd9af17
RogerMi12 <82840097+RogerMi12@users.noreply.github.com>
2025-04-28 20:10:54 -0500

    Update Seleccion.java

--- project/src/main/java/com/spring/project/entity/Seleccion.java
+++ project/src/main/java/com/spring/project/entity/Seleccion.java
@@ -1,26 +1,38 @@
 package com.spring.project.entity;
 
-import jakarta.persistence.*;
-import lombok.Data;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
-@Data
-@Table(name="selecciones")
+@Table(name = "seleccion")
 public class Seleccion {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	
-	private String nombre;
 
-	
-   
-	private int continenteID;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
+    private String nombre;
+    private String grupo;
 
-	private String grupo;
+    @ManyToOne
+    @JoinColumn(name = "continente_id")
+    private Continente continente;
 
+    // Getters y Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getGrupo() { return grupo; }
+    public void setGrupo(String grupo) { this.grupo = grupo; }
+
+    public Continente getContinente() { return continente; }
+    public void setContinente(Continente continente) { this.continente = continente; }
 }
-
