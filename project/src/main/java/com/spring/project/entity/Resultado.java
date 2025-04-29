43c06294da34ed6a4e15fc300053d45849a86d6a
RogerMi12 <82840097+RogerMi12@users.noreply.github.com>
2025-04-28 20:11:11 -0500

    Update Resultado.java

--- project/src/main/java/com/spring/project/entity/Resultado.java
+++ project/src/main/java/com/spring/project/entity/Resultado.java
@@ -1,30 +1,49 @@
 package com.spring.project.entity;
 
-import jakarta.persistence.*;
-import lombok.Data;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
-@Data
-@Table(name="resultado")
+@Table(name = "resultado")
 public class Resultado {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
-    
-  
-	private Integer partidoID;
+    private int goles;
+    private int amarillas;
+    private int rojas;
 
-    
-    
-    private Integer seleccionID;
-    
-    private Integer goles;
-    private Integer amarillas;
-    private Integer rojas;
+    @ManyToOne
+    @JoinColumn(name = "partido_id")
+    private Partido partido;
 
+    @ManyToOne
+    @JoinColumn(name = "seleccion_id")
+    private Seleccion seleccion;
+
+    // Getters y Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public int getGoles() { return goles; }
+    public void setGoles(int goles) { this.goles = goles; }
+
+    public int getAmarillas() { return amarillas; }
+    public void setAmarillas(int amarillas) { this.amarillas = amarillas; }
+
+    public int getRojas() { return rojas; }
+    public void setRojas(int rojas) { this.rojas = rojas; }
+
+    public Partido getPartido() { return partido; }
+    public void setPartido(Partido partido) { this.partido = partido; }
+
+    public Seleccion getSeleccion() { return seleccion; }
+    public void setSeleccion(Seleccion seleccion) { this.seleccion = seleccion; }
 }
-
